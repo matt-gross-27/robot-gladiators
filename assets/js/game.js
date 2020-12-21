ccca7160f75847ea774c2af702e21e96f3d70ee4
Matt Gross <mbgross111@gmail.com>
2020-12-21 15:33:00 -0800

    add fight function, test if robots are alive

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,48 @@
-// this creates a function named "fight"
-function fight() {
-  window.alert("the fight had begun!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName + ": ", "Attack: " + playerAttack, "Health: " + playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  // alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
+  
+  // subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
+  
+  // log a resulting message to the console so we know that it worked.
+  console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+
+  // check enemy's health
+  if(enemyHealth <= 0) {
+    window.alert(enemyName + " has been vanquished!");
+  }
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  // subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack
+  
+  // log a resulting message to the console so we know that it worked.
+  console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " remaining."
+  );
+
+  // check player's health
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has been defeated!");
+  }
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
 }
 
-var playerName = window.prompt("What is your robot's name?");
-
-console.log("playerName = " + playerName);
+fight();

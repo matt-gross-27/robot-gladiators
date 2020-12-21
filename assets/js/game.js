13aaec2ba0777b656c87f9ae9aac3a0828a735f6
Matt Gross <mbgross111@gmail.com>
2020-12-21 13:56:33 -0800

    add window alert and playerName Var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+function fight() {
+  window.alert("the fight had begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("playerName = " + playerName);

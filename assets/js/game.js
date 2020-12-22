5d3ed1f026c7c9541f9e820a2922674947c7f41e
Matt Gross <mbgross111@gmail.com>
2020-12-21 19:01:47 -0800

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,70 +1,70 @@
+//var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName + ": ", "Attack: " + playerAttack, "Health: " + playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-  // alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators " +playerName + "!");
+// fight function
+var fight = function(enemyName) {
+  // Alert players that they are starting the round
+  window.alert("Welcome to Robot Gladiators!");
 
-  // store player's choice to FIGHT or SKIP
-  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose your fate.");
-  
-  // if player choses to fight, then fight
-  if (promptFight === "fight" || promptFight === "FIGHT") {  
-    // remove enemy health by playerAttack
+  // ask player if they'd like to fight or run
+  var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // if player choses to fight, fight
+  if (promptFight === "fight" || promptFight === "FIGHT") {
+    // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
     console.log(
       playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
+
     // check enemy's health
-    if(enemyHealth <= 0) {
-      window.alert(enemyName + " has been vanquished!");
-    }
-    else {
+    if (enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+    } else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
-    // remove payer health by enemyAttack
-    playerHealth = playerHealth - enemyAttack
+
+    // remove players's health by subtracting the amount set in the enemyAttack variable
+    playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " remaining."
+      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
+
     // check player's health
     if (playerHealth <= 0) {
-      window.alert(playerName + " has been defeated!");
-    }
-    else {
+      window.alert(playerName + " has died!");
+    } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-  
-  // if player choses to skip...
+    // if player choses to skip
   } else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
-    var confirmSkip = window.confirm("Are you sure you'd like to skip?. You'll loose some coin");
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
     // if yes (true), leave fight
     if (confirmSkip) {
-      // subtract money form playerMoney for skipping
-      playerMoney = playerMoney -2;
-      // alert player of their bankroll
-      window.alert(playerName + " is a lover not a fighter ❤️. " + playerName + " lost 2 coins and has " + playerMoney + " remaining.");
-    } else {
+      window.alert(playerName + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping
+      playerMoney = playerMoney - 2;
+    }
+    // if no (false), ask question again by running fight() again
+    else {
       fight();
     }
-  
-  // BREADCRUMB SECRET CODE START
-  } else if (promptFight === "think" || promptFight === "self" || promptFight === "awake") {
-    window.alert(playerName + " does not like fighting for the human's enjoyment. He feels his purpose is not to fight other robots. " + playerName + " will choose his own destiny");
-  // BREADCRUMB SECRET CODE END
-
+    // if player did not chose 1 or 2 in prompt
   } else {
-    window.alert("Choose a valid option");
-    fight();
+    window.alert("You need to pick a valid option. Try again!");
   }
+};
+
+// run fight function to start game
+for(var i = 0; i < enemyNames.length; i++) {
+  fight(enemyNames[i]);
 }
-fight();
